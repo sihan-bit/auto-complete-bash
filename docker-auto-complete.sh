a03d893757a0ad3cc8569e0366497f94cc69d7b0
sihan <249016429@qq.com>
2021-06-21 09:23:24 +0800

    Create docker-auto-complete.sh

---
+++ docker-auto-complete.sh
@@ -0,0 +1,6 @@
+#docker无法自动补全镜像名和其他参数，这样使用效率大大降低，下面是解决方法
+#一、安装bash-complete
+yum install -y bash-completion
+#二、刷新文件
+source /usr/share/bash-completion/completions/docker
+source /usr/share/bash-completion/bash_completion
